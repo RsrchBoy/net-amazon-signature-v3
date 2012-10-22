759c672e6985eb003123f95cb13c437a4d590adb
Chris Weyl <cweyl@alumni.drew.edu>
2012-10-21 20:30:22 -0700

    initial commit

---
+++ lib/Net/Amazon/Signature/V3.pm
@@ -0,0 +1,80 @@
+package Net::Amazon::Signature::V3;
+
+# ABSTRACT: Sign AWS requests -- V3
+
+use utf8;
+
+use Moose;
+use namespace::autoclean;
+use MooseX::Types::Common::String ':all';
+
+use HTTP::Date;
+use Digest::HMAC_SHA1;
+use MIME::Base64;
+
+has $_ => (is => 'ro', isa => NonEmptySimpleStr, required => 1)
+    for qw{ id key };
+
+# see route53-dg-20120229.pdf, page 98
+sub _time_string { time2str time }
+
+=method signed_headers
+
+Returns a list of several key-value pairs suitable for including directly as
+headers.  These headers will authenticate the request to Amazon.
+
+Note that these headers are only good when used within 5 minutes of the time
+that Amazon thinks it is.
+
+This routine is largely based off code extracted from
+L<Net::Amazon::Route53/request>.
+
+=cut
+
+sub signed_headers {
+    my $self = shift @_;
+
+    my $hmac = Digest::HMAC_SHA1->new($self->key);
+    my $date = $self->_time_string;
+
+    $hmac->add($date);
+    my $signature = encode_base64($hmac->digest, '');
+
+    my $auth_string
+        = 'AWS3-HTTPS '
+        . 'AWSAccessKeyId=' . $self->id . ','
+        . 'Signature=' . $signature
+        ;
+
+    my %options = (
+        'x-amz-date'           => $date,
+        'X-Amzn-Authorization' => $auth_string,
+    );
+
+    ### %options
+    return %options;
+}
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+    # somewhere inside the depths of your code...
+    my $signer = Net::Amazon::Signature::V3->new(id => $id, key => $key);
+    my $req = HTTP::Request->new('GET', $uri, [ $signer->signed_headers ]);
+
+    # profit!
+
+=head1 DESCRIPTION
+
+Amazon requires authentication when interfacting with its web services; this
+package implements V3 of Amazon's authentication schemes.
+
+=head1 SEE ALSO
+
+L<Net::Amazon::Route53> -- signed_headers() is largely based off of the code
+inside L<Net::Amazon::Route53/request>.
+
+=cut
